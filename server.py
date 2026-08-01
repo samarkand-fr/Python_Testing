2ec384fffad598e69d0fd282665a4d055714523c
Dina <dina@macbook-pro-de-heba.home>
2026-08-01 08:02:19 +0200

    Feature #7: Implement public points display board

--- server.py
+++ server.py
@@ -84,3 +84,7 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+@app.route('/points')
+def points():
+    return render_template('points.html', clubs=clubs)
